582f7ae0b50e2f84a9d8715c9a5d8a7748eaf1d2
jarb29 <jarb29@gmail.com>
2020-06-22 20:55:02 -0400

    Updates

---
+++ precache-manifest.4477443bc609fe9f3086b48c46c883a5.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa370c0654619092c81f897a9d0ee7d",
+    "url": "/raizadelishop/index.html"
+  },
+  {
+    "revision": "ed05529edf831632d439",
+    "url": "/raizadelishop/static/css/main.05546d48.chunk.css"
+  },
+  {
+    "revision": "cfa00fc650e4c460b804",
+    "url": "/raizadelishop/static/js/2.16b19a3e.chunk.js"
+  },
+  {
+    "revision": "6d98567bd9d21bb7755c06ab8451d465",
+    "url": "/raizadelishop/static/js/2.16b19a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed05529edf831632d439",
+    "url": "/raizadelishop/static/js/main.1f542012.chunk.js"
+  },
+  {
+    "revision": "93eac50ba5de5e5a40ba",
+    "url": "/raizadelishop/static/js/runtime-main.8c6ca9d3.js"
+  },
+  {
+    "revision": "6735d13c5fee0ff9b41ad5048ab00a31",
+    "url": "/raizadelishop/static/media/bg18.6735d13c.jpg"
+  },
+  {
+    "revision": "099010a9c937f66c8ad6294795fa9dc4",
+    "url": "/raizadelishop/static/media/bg19.099010a9.jpg"
+  },
+  {
+    "revision": "a4ee339c6e002765df1197bfc0655ecf",
+    "url": "/raizadelishop/static/media/bg24.a4ee339c.jpg"
+  },
+  {
+    "revision": "7065ec594837d44f1fc436a513cbbd56",
+    "url": "/raizadelishop/static/media/bg25.7065ec59.jpg"
+  },
+  {
+    "revision": "ce30d42f9c3cba8e9f96a2d9c5749ac1",
+    "url": "/raizadelishop/static/media/bg26.ce30d42f.jpg"
+  },
+  {
+    "revision": "345b7b4e387d78d7760c0e07f9e44356",
+    "url": "/raizadelishop/static/media/bg27.345b7b4e.jpg"
+  },
+  {
+    "revision": "0ad93274b257a783c8ffdfe7dd22246e",
+    "url": "/raizadelishop/static/media/bg28.0ad93274.jpg"
+  },
+  {
+    "revision": "5ee0ab852c5282321ddc2a62e44359ba",
+    "url": "/raizadelishop/static/media/bg29.5ee0ab85.jpg"
+  },
+  {
+    "revision": "60b4e289a76e6d3dc5936c090926d9f0",
+    "url": "/raizadelishop/static/media/bg32.60b4e289.jpg"
+  },
+  {
+    "revision": "302847fb1fefbf207e18e72ec999e6cc",
+    "url": "/raizadelishop/static/media/bg38.302847fb.jpg"
+  },
+  {
+    "revision": "882db5bcd7d14066e5538970377d2c4c",
+    "url": "/raizadelishop/static/media/bg39.882db5bc.jpg"
+  },
+  {
+    "revision": "7e15299316b214695b0258a0c6a5a666",
+    "url": "/raizadelishop/static/media/bg40.7e152993.jpg"
+  },
+  {
+    "revision": "f50c59527667a65e268b35e6a1ea2398",
+    "url": "/raizadelishop/static/media/bg41.f50c5952.jpg"
+  },
+  {
+    "revision": "7bd4e82cf42ef4000226f83bb679a269",
+    "url": "/raizadelishop/static/media/bg43.7bd4e82c.jpg"
+  },
+  {
+    "revision": "7bd4e82cf42ef4000226f83bb679a269",
+    "url": "/raizadelishop/static/media/bg45.7bd4e82c.jpg"
+  },
+  {
+    "revision": "296a2c6b0dd26a6c2fd4e179ad5df76c",
+    "url": "/raizadelishop/static/media/bg50.296a2c6b.jpg"
+  },
+  {
+    "revision": "8342f36b5b00cfeab8ca58e759cf1a6a",
+    "url": "/raizadelishop/static/media/bg51.8342f36b.jpg"
+  },
+  {
+    "revision": "e4ef9b62aa4ce65c022ed833e792162a",
+    "url": "/raizadelishop/static/media/bg52.e4ef9b62.jpg"
+  },
+  {
+    "revision": "5a8fc0b4d404bd3258886b16d9a86aab",
+    "url": "/raizadelishop/static/media/image_placeholder.5a8fc0b4.jpg"
+  },
+  {
+    "revision": "16f27579184b7ef66ba477bcde0cb47d",
+    "url": "/raizadelishop/static/media/james.16f27579.jpg"
+  },
+  {
+    "revision": "60b4e289a76e6d3dc5936c090926d9f0",
+    "url": "/raizadelishop/static/media/login.60b4e289.jpg"
+  },
+  {
+    "revision": "5c4fa3fb1c780f17b8210eae2e6b1883",
+    "url": "/raizadelishop/static/media/logo.5c4fa3fb.jpg"
+  },
+  {
+    "revision": "0b276fd5496499ade4c42321f61891b7",
+    "url": "/raizadelishop/static/media/michael.0b276fd5.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/raizadelishop/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/raizadelishop/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/raizadelishop/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/raizadelishop/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "5e57125afe41de77903e46f251b2b52f",
+    "url": "/raizadelishop/static/media/olivia.5e57125a.jpg"
+  },
+  {
+    "revision": "60b4e289a76e6d3dc5936c090926d9f0",
+    "url": "/raizadelishop/static/media/profile.60b4e289.jpg"
+  },
+  {
+    "revision": "dcd9a7b470f20a91b8a3bafe9052bdc4",
+    "url": "/raizadelishop/static/media/project14.dcd9a7b4.jpg"
+  },
+  {
+    "revision": "bdcbb30dc22bdfd0ad62f45160311dc2",
+    "url": "/raizadelishop/static/media/project6.bdcbb30d.jpg"
+  },
+  {
+    "revision": "f3593f839987a837fc88f37fa009e03e",
+    "url": "/raizadelishop/static/media/project8.f3593f83.jpg"
+  }
+]);
